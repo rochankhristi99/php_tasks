e16ca9301158b794a1d50c874d3fb6f7b34f50c4
rochankhristi99 <rochankhristi99@gmail.com>
2024-01-26 12:10:22 +0200

    Excercise 1 Completed

--- ex1.php
+++ ex1.php
@@ -1,3 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercise 1: Getting Started with PHP - Rochan</title>
+
+  </head>
+  <body>
+
+<h3>
+3.1 Write PHP code to output the following message
+Hello world! My name is "David"
+</h3>
+
 <?php
-echo "Hello World";
+echo "Hello world! My name is \"David\"";
 ?>
+
+<h3>
+3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an <h4> (heading 4) element.
+</h3>
+
+<?php
+$title = "PHP is intresting.";
+echo "<h4>$title</h4>"
+?>
+
+
+<h3>
+3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade, and it should look like this:
+</h3>
+
+<?php
+$g1=5;
+$g2=4;
+$g3=5;
+?>
+
+<table>
+    <tr>
+       <th>
+        S.N
+       </th>
+       <th>
+        Name
+       </th>
+       <th>
+        Grade
+       </th>
+    </tr>
+    <tr>
+        <td>
+            2
+        </td>
+        <td>
+            John
+        </td>
+        <td>
+        <?php echo $g1 ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            2
+        </td>
+        <td>
+            Alice
+        </td>
+        <td>
+        <?php echo $g2 ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            3
+        </td>
+        <td>
+            Bob
+        </td>
+        <td>
+        <?php echo $g3 ?>
+        </td>
+    </tr>
+</table>
+
+
+<h3>
+3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file
+</h3>
+
+<img src="images/img1.png" />
+
+  </body>
+</html>
